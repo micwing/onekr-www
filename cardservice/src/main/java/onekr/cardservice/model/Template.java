ed28c9c112adcd0b30d775ae35c825196dc164d7
Michael Wing <micwing@163.com>
2014-11-20 18:03:02 +0800

    template files

--- cardservice/src/main/java/onekr/cardservice/model/Template.java
+++ cardservice/src/main/java/onekr/cardservice/model/Template.java
@@ -4,9 +4,22 @@ import onekr.framework.type.LabeledEnum;
 
 public enum Template implements LabeledEnum{
 
-	t10006("WEDDING DAY"),
+	t10001("t10001"),
+	t10002("t10002"),
+	t10003("t10003"),
+	t10004("t10004"),
+	t10005("t10005"),
+	t10006("t10006"),
+	t10007("t10007"),
+	t10008("t10008"),
+	t10009("t10009"),
+	t10010("t10010"),
+	t10011("t10011"),
+	t10012("t10012"),
+	t10013("t10013"),
 	
-	t10007("喜结良缘");
+	t10015("t10015"),
+	t10016("t10016");
 	
 	String label;
 
